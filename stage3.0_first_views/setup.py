806626987bf264967fd19da10b4c528229e94cf2
Siddhant Wahal <swahal@enthought.com>
2022-05-17 22:02:23 -0500

    Add setup.py

---
+++ stage3.0_first_views/setup.py
@@ -0,0 +1,28 @@
+from os.path import abspath, dirname, join
+from setuptools import setup, find_packages
+
+HERE = dirname(abspath(__file__))
+
+PKG_NAME = "pycasa"
+
+info = {}
+init_file = join(HERE, PKG_NAME, "__init__.py")
+
+with open(init_file) as fp:
+    exec(fp.read(), globals(), info)
+
+setup(
+    name=PKG_NAME,
+    version=info["__version__"],
+    description='Hello world for TraitsUI views',
+    ext_modules=[],
+    packages=find_packages(),
+    data_files=[
+        (".", ["README.md"]),
+    ],
+    entry_points={
+        'console_scripts': [
+            f'pycasa = {PKG_NAME}.app.main:main',
+        ],
+    },
+)
