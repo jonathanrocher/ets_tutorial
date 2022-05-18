312f2982d54dfc2e90c6a3d006c630d8ce94de9b
Siddhant Wahal <swahal@enthought.com>
2022-05-17 22:21:07 -0500

    Improve tests for ImageFolder

--- stage3.0_first_views/pycasa/model/test_image_folder.py
+++ stage3.0_first_views/pycasa/model/test_image_folder.py
@@ -10,9 +10,18 @@ SAMPLE_IMG_DIR = join(TUTORIAL_DIR, "..", "sample_images")
 
 
 class TestImageFolder(unittest.TestCase):
+    def setUp(self):
+        self.filenames = ["IMG-0311_xmas_2020.JPG", "owls.jpg"]
+        self.img_folder = ImageFolder(directory=SAMPLE_IMG_DIR)
+
     def test_filters_images(self):
-        img_folder = ImageFolder(directory=SAMPLE_IMG_DIR)
         self.assertEqual(
-            sorted([split(img.filepath)[1] for img in img_folder.images]),
-            sorted(["IMG-0311_xmas_2020.JPG", "owls.jpg"])
+            {split(img.filepath)[1] for img in self.img_folder.images},
+            set(self.filenames)
         )
+
+    def test_create_metadata_df(self):
+        df = self.img_folder.create_metadata_df()
+        self.assertEqual(set(df.index), set(self.filenames))
+        self.assertIn("ExifVersion", df)
+        self.assertIn("ApertureValue", df)
