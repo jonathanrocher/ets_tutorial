89adfff6a0ddbf401f61475a6fb5b1634c7801b5
Jonathan Rocher <jrocher@kbibiopharma.com>
2022-05-29 19:37:48 -0500

    Store data and simplify face detection method.

--- stage5.2_first_application/pycasa/model/image_file.py
+++ stage5.2_first_application/pycasa/model/image_file.py
@@ -4,11 +4,11 @@ import PIL.Image
 from PIL.ExifTags import TAGS
 from skimage import data
 from skimage.feature import Cascade
-import matplotlib.pyplot as plt
 import numpy as np
 
 # ETS imports
-from traits.api import cached_property, Dict, File, HasStrictTraits, Property
+from traits.api import Array, cached_property, Dict, File, HasStrictTraits, \
+    Property
 
 SUPPORTED_FORMATS = [".png", ".jpg", ".jpeg"]
 
@@ -20,6 +20,8 @@ class ImageFile(HasStrictTraits):
 
     metadata = Property(Dict, depends_on="filepath")
 
+    data = Property(Array, depends_on="filepath")
+
     def to_array(self):
         file_ext = os.path.splitext(self.filepath)[1].lower()
         if not self.filepath or file_ext not in SUPPORTED_FORMATS:
@@ -27,6 +29,10 @@ class ImageFile(HasStrictTraits):
 
         with PIL.Image.open(self.filepath) as img:
             return np.asarray(img)
+
+    @cached_property
+    def _get_data(self):
+        return self.to_array()
 
     @cached_property
     def _get_metadata(self):
@@ -43,20 +49,15 @@ class ImageFile(HasStrictTraits):
         else:
             return {}
 
-    def detect_faces(self):
+    def detect_faces(self, scale_factor=1.2, step_ratio=1, min_size=60,
+                     max_size=600):
         """ Detect faces in the image.
         """
-        # Load the trained file from the module root.
         trained_file = data.lbp_frontal_face_cascade_filename()
-
-        # Initialize the detector cascade.
         detector = Cascade(trained_file)
-
-        img = plt.imread(self.filepath)
-
-        faces = detector.detect_multi_scale(img=img,
-                                            scale_factor=1.2,
-                                            step_ratio=1,
-                                            min_size=(60, 60),
-                                            max_size=(600, 600))
+        faces = detector.detect_multi_scale(img=self.data,
+                                            scale_factor=scale_factor,
+                                            step_ratio=step_ratio,
+                                            min_size=(min_size, min_size),
+                                            max_size=(max_size, max_size))
         return faces
