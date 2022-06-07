5bb789478a1d4d184c913065778ff4b462eb4396
Siddhant Wahal <swahal@enthought.com>
2022-06-06 20:15:46 -0500

    Allow cancelling

--- stage5.2_fuller_application/pycasa/ui/image_folder_view.py
+++ stage5.2_fuller_application/pycasa/ui/image_folder_view.py
@@ -1,7 +1,7 @@
 # General imports
 
 # ETS imports
-from traits.api import Button, Instance, observe
+from traits.api import Bool, Button, Instance, observe
 from traitsui.api import HGroup, Item, Label, ModelView, Spring, View
 from traitsui.ui_editors.data_frame_editor import DataFrameEditor
 # Local imports
@@ -12,6 +12,10 @@ class ImageFolderView(ModelView):
     """ ModelView for an image folder object.
     """
     model = Instance(ImageFolder)
+
+    cancel = Button("Cancel")
+
+    _cancel_clicked = Bool
 
     scan = Button("Scan for faces...")
 
@@ -32,6 +36,11 @@ class ImageFolderView(ModelView):
                 show_label=False,
                 enabled_when="len(model.data) > 0 and model.executor_idle",
             ),
+            Item(
+                "cancel",
+                show_label=False,
+                enabled_when="not model.executor_idle and not _cancel_clicked",
+            ),
             Spring(),
         )
     )
@@ -39,3 +48,13 @@ class ImageFolderView(ModelView):
     @observe("scan")
     def scan_for_faces(self, event):
         self.model.compute_num_faces_background()
+
+    @observe("cancel")
+    def _cancel_scan(self, event):
+        self.model.future.cancel()
+        self._cancel_clicked = True
+
+    @observe("model:future:done")
+    def _reset_click_counter(self, event):
+        if self.model.future.done:
+            self._cancel_clicked = False
