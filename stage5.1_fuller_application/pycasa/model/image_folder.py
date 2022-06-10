304f03d13951112fbcd011923d29fdd57c4d4401
Jonathan Rocher <jrocher@kbibiopharma.com>
2022-06-09 21:48:37 -0500

    Add __init__ method for model for education purpose and bring in unittests tests.

--- stage5.1_fuller_application/pycasa/model/image_folder.py
+++ stage5.1_fuller_application/pycasa/model/image_folder.py
@@ -1,12 +1,16 @@
+
+# General imports
 import glob
-from os.path import expanduser, split
+from os.path import expanduser, isdir, split
 
 import pandas as pd
 
+# ETS imports
 from traits.api import (
     Directory, HasStrictTraits, Instance, List, observe,
 )
 
+# Local imports
 from pycasa.model.image_file import ImageFile
 
 SUPPORTED_FORMATS = [".png", ".jpg", ".jpeg", ".PNG", ".JPG", ".JPEG"]
@@ -18,6 +22,14 @@ class ImageFolder(HasStrictTraits):
     directory = Directory(expanduser("~"))
 
     images = List(Instance(ImageFile))
+
+    def __init__(self, **traits):
+        super(ImageFolder, self).__init__(**traits)
+        import pdb ; pdb.set_trace()
+        if not isdir(self.directory):
+            msg = f"The provided directory isn't a real directory: " \
+                  f"{self.directory}"
+            raise ValueError(msg)
 
     @observe("directory")
     def _get_images(self, event):
--- stage5.1_fuller_application/pycasa/model/tests/test_image_folder.py
+++ stage5.1_fuller_application/pycasa/model/tests/test_image_folder.py
@@ -1,37 +1,31 @@
-from os.path import dirname, join
-from unittest import TestCase
-
-import pandas as pd
+from os.path import dirname, join, split
+import unittest
 
 from pycasa.model.image_folder import ImageFolder
 
 import ets_tutorial
 
 TUTORIAL_DIR = dirname(ets_tutorial.__file__)
-
 SAMPLE_IMG_DIR = join(TUTORIAL_DIR, "..", "sample_images")
 
-HERE = dirname(__file__)
 
-
-class TestImageFolder(TestCase):
-    def test_no_folder(self):
-        with self.assertRaises(ValueError):
-            ImageFolder()
+class TestImageFolder(unittest.TestCase):
+    def setUp(self):
+        self.filenames = ["IMG-0311_xmas_2020.JPG", "owls.jpg"]
+        self.img_folder = ImageFolder(directory=SAMPLE_IMG_DIR)
 
     def test_with_file(self):
         with self.assertRaises(ValueError):
-            ImageFolder(path=__file__)
+            ImageFolder(directory=__file__)
 
-    def test_empty_folder(self):
-        img = ImageFolder(path=HERE)
-        data = img.to_dataframe()
-        self.assertIsInstance(data, pd.DataFrame)
-        self.assertEqual(len(data), 0)
+    def test_filters_images(self):
+        self.assertEqual(
+            {split(img.filepath)[1] for img in self.img_folder.images},
+            set(self.filenames)
+        )
 
-    def test_real_folder(self):
-        img = ImageFolder(path=SAMPLE_IMG_DIR)
-        data = img.to_dataframe()
-        self.assertEqual(len(data), 2)
-        for key in ['ExifVersion', 'ExifImageWidth', 'ExifImageHeight']:
-            self.assertIn(key, data.columns)
+    def test_create_metadata_df(self):
+        df = self.img_folder.create_metadata_df()
+        self.assertEqual(set(df.index), set(self.filenames))
+        self.assertIn("ExifVersion", df)
+        self.assertIn("ApertureValue", df)
