93c8c8ea3c00eb186042ce595f98feed64dda706
Jonathan Rocher <jrocher@kbibiopharma.com>
2022-06-12 12:41:10 -0500

    Structure stage 0 better.

---
+++ stage0_starting_script/face_detect.py
@@ -0,0 +1,60 @@
+""" Script analyzing an image, detecting human faces inside it, and printing
+EXIF data about it.
+"""
+from os.path import join
+
+import PIL.Image
+from PIL.ExifTags import TAGS
+from skimage import data
+from skimage.feature import Cascade
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import patches
+
+# Select image file -----------------------------------------------------------
+
+image_path = join("..", "sample_images", "IMG-0311_xmas_2020.JPG")
+image_path2 = join("..", "sample_images", "owls.jpg")
+
+for path in [image_path, image_path2]:
+    img = PIL.Image.open(path)
+
+    img_metadata = {TAGS[k]: v for k, v in img._getexif().items() if k in TAGS}
+
+    # Detect faces ------------------------------------------------------------
+
+    # Load the trained file from the module root.
+    trained_file = data.lbp_frontal_face_cascade_filename()
+
+    # Initialize the detector cascade.
+    detector = Cascade(trained_file)
+
+    detected = detector.detect_multi_scale(img=np.asarray(img),
+                                           scale_factor=1.2,
+                                           step_ratio=1,
+                                           min_size=(60, 60),
+                                           max_size=(600, 600))
+
+    img_metadata["Number of faces detected"] = len(detected)
+    print(img_metadata)
+
+    # Visualize results -------------------------------------------------------
+
+    plt.imshow(img)
+    img_desc = plt.gca()
+    plt.set_cmap('gray')
+
+    for patch in detected:
+
+        img_desc.add_patch(
+            patches.Rectangle(
+                (patch['c'], patch['r']),
+                patch['width'],
+                patch['height'],
+                fill=False,
+                color='r',
+                linewidth=2
+            )
+        )
+
+    plt.show()
