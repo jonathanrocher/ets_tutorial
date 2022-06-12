72887e88109014068fe2daba9825ac00bd3b3ecd
Siddhant Wahal <swahal@enthought.com>
2022-06-12 14:30:11 -0500

    Add tests

--- stage5.2_fuller_application/pycasa/model/tests/test_image_folder.py
+++ stage5.2_fuller_application/pycasa/model/tests/test_image_folder.py
@@ -4,7 +4,10 @@ from unittest import TestCase
 import numpy as np
 import pandas as pd
 
-from pycasa.model.image_folder import ImageFolder
+from pyface.toolkit import toolkit_object
+from traits_futures.api import TraitsExecutor
+
+from pycasa.model.image_folder import ImageFolder, NUM_FACE_COL
 
 import ets_tutorial
 
@@ -14,8 +17,22 @@ SAMPLE_IMG_DIR = join(TUTORIAL_DIR, "..", "sample_images")
 
 HERE = dirname(__file__)
 
+# Maximum timeout for blocking calls.
+SAFETY_TIMEOUT = 300
 
-class TestImageFolder(TestCase):
+# GuiTestAssistant is only available for Qt.
+GuiTestAssistant = toolkit_object("util.gui_test_assistant:GuiTestAssistant")
+
+
+class TestImageFolder(GuiTestAssistant, TestCase):
+    def setUp(self):
+        GuiTestAssistant.setUp(self)
+        self.traits_executor = TraitsExecutor()
+
+    def tearDown(self):
+        self.traits_executor.shutdown(timeout=SAFETY_TIMEOUT)
+        GuiTestAssistant.tearDown(self)
+
     def test_no_folder(self):
         with self.assertRaises(ValueError):
             ImageFolder()
@@ -36,3 +53,16 @@ class TestImageFolder(TestCase):
         self.assertEqual(len(data), 2)
         for key in ['ExifVersion', 'ExifImageWidth', 'ExifImageHeight']:
             self.assertIn(key, data.columns)
+
+    def test_compute_num_faces_background(self):
+        image_folder = ImageFolder(
+            path=SAMPLE_IMG_DIR,
+            traits_executor=self.traits_executor
+        )
+        image_folder.compute_num_faces_background()
+        self.assertEventuallyTrueInGui(
+            lambda: image_folder.future.done, timeout=SAFETY_TIMEOUT
+        )
+        self.assertEqual(
+            image_folder.data[NUM_FACE_COL].to_list(), [5.0, 3.0]
+        )
