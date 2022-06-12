0eb2e9b4ff761b5a800bf78495717286939ccf00
Siddhant Wahal <swahal@enthought.com>
2022-06-12 14:54:34 -0500

    Revert "Veto closing tab if computation active"
    This reverts commit a22fdf5bd2580ce0540103b583b59be4d706f0b4.

--- stage5.2_fuller_application/pycasa/ui/image_folder_editor.py
+++ stage5.2_fuller_application/pycasa/ui/image_folder_editor.py
@@ -1,5 +1,4 @@
-from traits.api import Property, observe
-from pyface.api import warning
+from traits.api import Property
 from pyface.tasks.api import Editor
 
 from .image_folder_view import ImageFolderView
@@ -35,9 +34,3 @@ class ImageFolderEditor(Editor):
 
     def _get_tooltip(self):
         return self.obj.path
-
-    @observe("closing")
-    def _veto_if_computing(self, event):
-        event.new.veto = not self.obj.executor_idle
-        if event.new.veto:
-            warning(self.control, "Please wait for computation to finish!")
