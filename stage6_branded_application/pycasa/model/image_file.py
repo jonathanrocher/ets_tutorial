d88651e09052ecff05b281e82b8b582fc151cde9
Jonathan Rocher <jrocher@kbibiopharma.com>
2022-07-12 05:25:56 -0500

    Re-align models with previous stages.

--- stage6_branded_application/pycasa/model/image_file.py
+++ stage6_branded_application/pycasa/model/image_file.py
@@ -1,5 +1,5 @@
 # General imports
-import os
+from os.path import splitext
 import PIL.Image
 from PIL.ExifTags import TAGS
 from skimage import data
@@ -7,10 +7,11 @@ from skimage.feature import Cascade
 import numpy as np
 
 # ETS imports
-from traits.api import Array, cached_property, Dict, File, HasStrictTraits, \
-    List, Property
+from traits.api import (
+    Array, cached_property, Dict, File, HasStrictTraits, List, Property
+)
 
-SUPPORTED_FORMATS = [".png", ".jpg", ".jpeg"]
+SUPPORTED_FORMATS = [".png", ".jpg", ".jpeg", ".PNG", ".JPG", ".JPEG"]
 
 
 class ImageFile(HasStrictTraits):
@@ -24,43 +25,40 @@ class ImageFile(HasStrictTraits):
 
     faces = List
 
-    def to_array(self):
-        file_ext = os.path.splitext(self.filepath)[1].lower()
-        if not self.filepath or file_ext not in SUPPORTED_FORMATS:
-            return np.array([])
+    def _is_valid_file(self):
+        return (
+            bool(self.filepath) and
+            splitext(self.filepath)[1].lower() in SUPPORTED_FORMATS
+        )
 
+    @cached_property
+    def _get_data(self):
+        if not self._is_valid_file():
+            return np.array([])
         with PIL.Image.open(self.filepath) as img:
             return np.asarray(img)
 
     @cached_property
-    def _get_data(self):
-        return self.to_array()
-
-    @cached_property
     def _get_metadata(self):
-        file_ext = os.path.splitext(self.filepath)[1].lower()
-        if not self.filepath or file_ext not in SUPPORTED_FORMATS:
+        if not self._is_valid_file():
             return {}
-
         with PIL.Image.open(self.filepath) as img:
             exif = img._getexif()
-
-        if exif:
-            return {TAGS[k]: v for k, v in exif.items()
-                    if k in TAGS}
-        else:
+        if not exif:
             return {}
+        return {TAGS[k]: v for k, v in exif.items() if k in TAGS}
 
-    def detect_faces(self, scale_factor=1.2, step_ratio=1, min_size=60,
-                     max_size=600):
-        """ Detect faces in the image.
-        """
+    def detect_faces(self):
+        # Load the trained file from the module root.
         trained_file = data.lbp_frontal_face_cascade_filename()
+
+        # Initialize the detector cascade.
         detector = Cascade(trained_file)
-        faces = detector.detect_multi_scale(img=self.data,
-                                            scale_factor=scale_factor,
-                                            step_ratio=step_ratio,
-                                            min_size=(min_size, min_size),
-                                            max_size=(max_size, max_size))
-        self.faces = faces
-        return faces
+
+        detected = detector.detect_multi_scale(img=self.data,
+                                               scale_factor=1.2,
+                                               step_ratio=1,
+                                               min_size=(60, 60),
+                                               max_size=(600, 600))
+        self.faces = detected
+        return self.faces
