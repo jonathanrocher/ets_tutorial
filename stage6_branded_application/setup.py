a2ec904bf569f53e9ddfb920f4bb8c9b57884ac3
Jonathan Rocher <jrocher@kbibiopharma.com>
2022-07-11 21:35:06 -0500

    Move entry point and full setup to stage 8.

--- stage6_branded_application/setup.py
+++ stage6_branded_application/setup.py
@@ -1,27 +1,14 @@
-from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
-
-HERE = dirname(abspath(__file__))
-
-PKG_NAME = "pycasa"
-
-info = {}
-init_file = join(HERE, PKG_NAME, "__init__.py")
-exec(open(init_file).read(), globals(), info)
 
 
 setup(
-    name=PKG_NAME,
-    version=info["__version__"],
-    description='Hello world in pyface task',
+    name="pycasa",
+    version="0.0.1",
+    description='ETS based GUI application for image exploration and face '
+                'detection',
     ext_modules=[],
     packages=find_packages(),
     data_files=[
         (".", ["README.md"]),
     ],
-    entry_points={
-        'console_scripts': [
-            'pycasa = {}.app.main:main'.format(PKG_NAME),
-        ],
-    },
 )
